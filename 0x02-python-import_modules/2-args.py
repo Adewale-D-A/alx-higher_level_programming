2c22a3105bb59d55d469e729f47e507f2b8f0b37
adewale-d-a <adewale-d-a.com>
2022-09-25 13:26:27 +0100

    2 args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
+    i = len(sys.argv) - 1
 
-if __name__ != "__main__":
-    exit()
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
 
-argumentStr = "{:d} argument"
-argc = len(sys.argv) - 1
-if argc == 0:
-    argumentStr += 's.'
-elif argc == 1:
-    argumentStr += ':'
-else:
-    argumentStr += 's:'
-print(argumentStr.format(argc))
-
-i = 0
-for argument in sys.argv:
-    if i != 0:
-        print("{:d}: {:s}".format(i, argument))
-    i += 1
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
