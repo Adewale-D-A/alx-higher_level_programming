a4c462e99c62c42acccb12164c4c264ee97da563
adewale-d-a <adewale-d-a.com>
2022-10-17 23:48:42 +0100

    task 0 redo

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 class Rectangle:
-    """Empty class Rectangle that defines a rectangle figure
-    Attributes:
-        empty
-    """
-
+    """ Empty class """
     pass
