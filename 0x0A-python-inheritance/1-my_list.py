6fb831f7504aa4c4c6922ef2e98021210dccc25c
adewale-d-a <adewale-d-a.com>
2022-10-24 20:52:37 +0100

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-class MyList(list):
-    """ Class that inherits the attributes references of class list
-    Args:
-        list: class list
-    """
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 8:47:00 2022
+@author: Adewale
+"""
 
+
+class MyList(list):
+    """
+     class MyList that inherits from list
+    """
     def print_sorted(self):
-        """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+        """
+        Public instance method that prints sorted list
+        """
+        list_copy = self[:]
+        list_copy.sort()
+        print(list_copy)
